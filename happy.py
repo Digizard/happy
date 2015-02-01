47ac06ef01988c4e510cbede89d9ab2ddc3b6c16
SittingFox <Grimmjam@gmail.com>
2015-02-01 10:55:43 -0500

    Update happy.py

--- happy.py
+++ happy.py
@@ -1,6 +1,7 @@
 import sys, pygame
 
 screen = None
+screen_rect = None
 
 clock = pygame.time.Clock()
 
@@ -29,6 +30,9 @@ modules = {
     'scrap': False
 }
 
+#
+# The actual running portion of happy. Contains the active game loop.
+#
 def start():
     # Setup happy default values
     setup()
@@ -45,29 +49,36 @@ def start():
         
         # Handle draw
         screen.fill(BLACK)
-        draw()
+        draw_rect = draw()
         
-        pygame.display.update()
+        if draw_rect == None:
+			draw_rect = screen_rect
+        pygame.display.update(draw_rect)
 
+#
+# Handles the initial setup of the game. It allows changes from the conf
+# function to take effect and applies them.
+#
 def setup():
-    global window, modules
+    global window, modules, draw_rects
     window, modules = conf(window, modules)
     pygame.display.init()
 
-    setupWindow()
-    setupModules()
+    setup_window()
+    setup_modules()
 
-def setupWindow():
-    global screen
-    screenSize = (window['width'], window['height'])
-    screenFlag = 0
+def setup_window():
+    global screen, screen_rect
+    screen_size = (window['width'], window['height'])
+    screen_flag = 0
     
+    # Assumes you cannot do (or would not want) to do more than one.
     if window['fullscreen']:
-        screenFlag = pygame.FULLSCREEN
+        screen_flag = pygame.FULLSCREEN
     elif window['resizeable']:
-        screenFlag = pygame.RESIZABLE
+        screen_flag = pygame.RESIZABLE
     elif window['borderless']:
-        screenFlag = pygame.NOFRAME
+        screen_flag = pygame.NOFRAME
     		
     pygame.display.set_caption(window['title'])
     
@@ -75,9 +86,10 @@ def setupWindow():
         icon = pygame.image.load(window['icon'])
         pygame.display.set_icon(icon)
 
-    screen = pygame.display.set_mode(screenSize, screenFlag)
+    screen = pygame.display.set_mode(screen_size),
+    screen_rect = pygame.Rect(0, 0, screen['width'], screen['height'])
   
-def setupModules():
+def setup_modules():
     if modules['cdrom']:
         pygame.cdrom.init()
     if modules['font']:
@@ -92,14 +104,30 @@ def setupModules():
         pygame.mixer.init()
     if modules['scrap']:
         pygame.scrap.init()
-	
 
+#
+# Allows the user to customize window settings (dimensions, title, etc.) and
+# which modules are actually used.
+#
+# Customizable through @happy.loop decoration.
+#
 def conf(window, modules):
     return window, modules
-    
+
+#
+# Optional place to initialize variables, declare variables, and call functions
+# for such prior to the start of the actual game loop.
+#
+# Customizable through @happy.loop decoration.
+#
 def load():
     return
 
+#
+# Shoots each event's parts to their appropriate function.
+#
+# Optionally customizable. Recommended to leave alone.
+#
 def events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -139,14 +167,22 @@ def update():
 def draw():
     return
         
-        
+#
+# For exiting the entire program.
+#
 def quit():
     pygame.quit()
     sys.exit()
 
+#
+# happy decorator function.
+#
+# @happy.loop to change any of the parts for the game loop. Use caps,
+# lowercase, or underscores to your preference and coding style.
+#
 def loop(function):
     function_name = function.__name__.lower()
-    underscore = "_"
+    underscore = '_'
     
     function_name = function_name.replace(underscore,'')
     
