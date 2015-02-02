873fb96d01e6da647d51b01daf32eee5058faa50
SittingFox <Grimmjam@gmail.com>
2015-02-01 19:50:09 -0500

    Update happy.py

--- happy.py
+++ happy.py
@@ -5,8 +5,31 @@ screen_rect = None
 
 clock = pygame.time.Clock()
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+color = {
+    # Extremes
+    'WHITE': (255, 255, 255),
+    'BLACK': (0, 0, 0),
+    'RED': (255, 0, 0),
+    'LIME': (0, 255, 0),
+    'BLUE': (0, 0, 255),
+
+    # Basics
+    'YELLOW': (255, 255, 0),
+    'CYAN': (0, 255, 255),
+    'MAGENTA': (255, 0 , 255),
+    'SILVER': (192, 192, 192),
+    'GRAY': (128, 128, 128),
+    'MAROON': (128, 0, 0),
+    'OLIVE': (128, 128, 0),
+    'GREEN': (0, 128, 0),
+    'PURPLE': (128, 0 ,128),
+    'TEAL': (0, 128, 128),
+    'NAVY': (0, 0, 128)
+}
+
+graphics = {
+
+}
 
 window = {
     'title': 'Untitled',
@@ -48,7 +71,7 @@ def start():
         update()
         
         # Handle draw
-        screen.fill(BLACK)
+        screen.fill(color['BLACK'])
         draw_rect = draw()
         
         if draw_rect == None:
